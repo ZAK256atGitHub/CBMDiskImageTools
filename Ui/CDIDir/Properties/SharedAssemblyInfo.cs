15b05d2778a9f40f48e5e0ecfaa69bd392dc7f69
Hans <Hans@DESKTOP-UAD368K>
2017-03-05 19:18:22 +0100

    SharedAssemblyInfo Link Corretion

--- Ui/CDIDir/Properties/SharedAssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
- 
-[assembly: AssemblyCompany("ZAK256")]
-[assembly: AssemblyProduct("CBM Disk Image Tools")]
-[assembly: AssemblyCopyright("Copyright © ZAK256 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("0.0.1.*")]
-[assembly: AssemblyInformationalVersion("0.0.1")]
