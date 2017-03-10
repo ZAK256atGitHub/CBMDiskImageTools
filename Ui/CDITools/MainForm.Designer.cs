183f4c4bffff476eb86164573a4f520b7bb16fd7
Hans <Hans@DESKTOP-UAD368K>
2017-03-10 21:53:05 +0100

    Form Form Form

---
+++ Ui/CDITools/MainForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace CDITools
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBoxDiskLabel = new System.Windows.Forms.TextBox();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9});
+            this.listView1.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.Location = new System.Drawing.Point(12, 56);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(868, 443);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(893, 27);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem1,
+            this.exportToolStripMenuItem});
+            this.toolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(41, 23);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(152, 24);
+            this.openToolStripMenuItem1.Text = "Open";
+            this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click_1);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Filename";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Size";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader2.Width = 45;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Splat";
+            this.columnHeader3.Width = 55;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Ext";
+            this.columnHeader4.Width = 45;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Lock";
+            this.columnHeader5.Width = 45;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Index";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader6.Width = 55;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Geos Type";
+            this.columnHeader7.Width = 85;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Geos Struct";
+            this.columnHeader8.Width = 100;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "MD5";
+            this.columnHeader9.Width = 280;
+            // 
+            // textBoxDiskLabel
+            // 
+            this.textBoxDiskLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDiskLabel.Location = new System.Drawing.Point(12, 30);
+            this.textBoxDiskLabel.Name = "textBoxDiskLabel";
+            this.textBoxDiskLabel.ReadOnly = true;
+            this.textBoxDiskLabel.Size = new System.Drawing.Size(868, 23);
+            this.textBoxDiskLabel.TabIndex = 2;
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.exportToolStripMenuItem.Text = "Export";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 509);
+            this.Controls.Add(this.textBoxDiskLabel);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip2;
+            this.Name = "MainForm";
+            this.Text = "CBM Disk Image Tool";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.TextBox textBoxDiskLabel;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+    }
+}
+
---
+++ Ui/CDITools/MainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Collections;
+using ZAK256.CBMDiskImageTools.Logic.Core;
+namespace CDITools
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            listView1.FullRowSelect = true;
+        }
+        private void openToolStripMenuItem1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "D64 image (*.D64)|*.d64|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listView1.Items.Clear();
+                listView1.Columns[0].TextAlign = HorizontalAlignment.Right;
+                string imagePathFilename = openFileDialog1.FileName;
+                byte[] bamBlock;
+                ArrayList dirEntryList = new ArrayList();
+                bamBlock = DOSDisk.ReadBAMBlock(imagePathFilename);
+                textBoxDiskLabel.Text = String.Format("0 \"{0}\" {1} {2}"
+                    , Core.ConvertPETSCII2ASCII(DOSDisk.GetDiskName(bamBlock))
+                    , Core.ConvertPETSCII2ASCII(DOSDisk.GetDiskID(bamBlock))
+                    , Core.ConvertPETSCII2ASCII(DOSDisk.GetDOSType(bamBlock)));
+
+                dirEntryList = DOSDisk.GetDirEntryList(bamBlock, imagePathFilename);
+                int dirIndex = 0;
+                foreach (byte[] de in dirEntryList)
+                {
+                    dirIndex++;
+                    if (DOSDisk.GetFileType(de) != 0)
+                    {
+                        ListViewItem listViewItem = new ListViewItem(Core.ConvertPETSCII2ASCII(DOSDisk.GetFilename(de)) + Core.ConvertPETSCII2ASCII(DOSDisk.GetPartAfterFilename(de))); // first col
+                        listViewItem.SubItems.Add(DOSDisk.GetFileSizeInBlocks(de).ToString());                        
+                        listViewItem.SubItems.Add(DOSDisk.GetSplatFileSign(de));
+                        listViewItem.SubItems.Add(DOSDisk.GetFileTypeExt(de));
+                        listViewItem.SubItems.Add(DOSDisk.GetLockFlagSign(de));
+                        listViewItem.SubItems.Add(dirIndex.ToString());
+                        listViewItem.SubItems.Add((GEOSDisk.IsGeosFile(de) ? GEOSDisk.GetGEOSFiletypeName(de) : "   "));
+                        listViewItem.SubItems.Add((GEOSDisk.IsGeosFile(de) ? GEOSDisk.GetGEOSFileStructureName(de) : "    "));
+                        listViewItem.SubItems.Add(DOSDisk.GetMD5ByFile(de, imagePathFilename));
+                        listView1.Items.Add(listViewItem);                        
+                    }
+                }
+            }
+        }
+    }
+}
